8bfe8edb17810d8ba56772b3b1961606a1be7a7d
Tim Stumbaugh <stum@getabine.com>
2012-10-18 17:22:01 -0400

    Add attributes for the Amazon S3 recipes.

---
+++ attributes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: abine
+#
+# Copyright 2012, Abine, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Attributes related to credentials
+default[:abine][:credentials][:s3_file] = "latest.tar.gz"
+default[:abine][:credentials][:aws_access_key] = ""
+default[:abine][:credentials][:aws_secret_key] = ""
+default[:abine][:credentials][:credential_location] = "/etc/apache2/php5/creds"
+default[:abine][:credentials][:s3_bucket] = ""
--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,34 @@ depends "rightscale"
 depends "web_apache"
 
 recipe "abine::install_all_creds", "Install a TAR of all the credentials"
+
+attribute "abine/credentials/s3_file",
+  :display_name => "Abine Credential Remote File Name",
+  :description => "The name of the credential file as it exists in an Amazon S3 bucket.",
+  :required => "required",
+  :recipes => ["abine::install_all_creds"]
+
+attribute "abine/credentials/aws_access_key",
+  :display_name => "Amazon Access Key that can read this file",
+  :description => "AWS credentials that have read access to the file that you specified.",
+  :required => "required",
+  :recipes => ["abine::install_all_creds"]
+
+attribute "abine/credentials/aws_secret_key",
+  :display_name => "Amazon Secret Access Key that can read this file",
+  :description => "AWS credentials that have read access to the file that you specified.",
+  :required => "required",
+  :recipes => ["abine::install_all_creds"]
+
+attribute "abine/credentials/credential_location",
+  :display_name => "Local Path for Credential Files",
+  :description => "Local Directory where credential files should live. Leave out trailing slash",
+  :required => "required",
+  :default => "/etc/php5/creds",
+  :recipes => ["abine::install_all_creds"]
+
+attribute "abine/credentials/s3_bucket",
+  :display_name => "Amazon S3 bucket",
+  :description => "Name of S3 bucket where the credentials are stored.",
+  :required => "required",
+  :recipes => ["abine::install_all_creds"]
